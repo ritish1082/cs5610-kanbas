00c26d6396a54a7a6d7640b652cac321700a685b
ritish <sritish.10@gmail.com>
2024-09-16 10:33:52 -0400

    fix: added name

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate } from "react-router";
+import { Routes, Route } from "react-router";
 import Lab1 from "./Lab1";
 import Lab2 from "./Lab2";
 import TOC from "./TOC";
@@ -6,11 +6,15 @@ import TOC from "./TOC";
 export default function Labs() {
   return (
     <div>
-      <h1>Ritish Reddy</h1>
-      <h1>Labs</h1>
+      <h1>Sai Ritish Reddy Musku</h1>
+      <p>Section 3</p>
+      <p>CRN: 20596</p>
+      <p>
+        Github code for CS 5610 <a href="https://github.com/ritish1082/cs5610-kanbas">here</a>
+      </p>
+      <h2>Labs</h2>
       <TOC />
       <Routes>
-        <Route path="/" element={<Navigate to="Lab1" />} />
         <Route path="Lab1" element={<Lab1 />} />
         <Route path="Lab2" element={<Lab2 />} />
         <Route path="Lab3" element={<h2>Lab 3</h2>} />
