5b9b9fcbbd0b36a6177f4fd87dfe0f6b6116fc2b
ritish <sritish.10@gmail.com>
2024-10-02 21:22:19 -0400

    revert: a1 assignment index.ts

--- src/Kanbas/Courses/Assignments/index.tsx
+++ src/Kanbas/Courses/Assignments/index.tsx
@@ -1,11 +1,45 @@
-import AssignmentsControls from "./AssignmentsControls";
-
 export default function Assignments() {
   return (
     <div id="wd-assignments">
-      <AssignmentsControls />
-      <h2>Assignments</h2>
-
+      <input id="wd-search-assignment" placeholder="Search for Assignments" />
+      <button id="wd-add-assignment-group">+ Group</button>
+      <button id="wd-add-assignment">+ Assignment</button>
+      <h3 id="wd-assignments-title">
+        ASSIGNMENTS 40% of Total <button>+</button>
+      </h3>
+      <ul id="wd-assignment-list">
+        <li className="wd-assignment-list-item">
+          <a className="wd-assignment-link" href="#/Kanbas/Courses/1234/Assignments/123">
+            A1 - ENV + HTML
+          </a>
+          <br />
+          <span>
+            {" "}
+            Multiple Modules | <b>Not available until</b> May 6 at 12:00am |<br /> <b>Due</b> May 13
+            at 11:59pm | 100 pts
+          </span>
+        </li>
+        <li className="wd-assignment-list-item">
+          <a className="wd-assignment-link" href="#/Kanbas/Courses/1234/Assignments/123">
+            A2 - CSS + BOOTSTRAP
+          </a>
+          <br />
+          <span>
+            Multiple Modules | <b>Not available until</b> May 13 at 12:00am |<br /> <b>Due</b> May
+            20 at 11:59pm | 100 pts
+          </span>
+        </li>
+        <li className="wd-assignment-list-item">
+          <a className="wd-assignment-link" href="#/Kanbas/Courses/1234/Assignments/123">
+            A3 - JAVASCRIPT + REACT
+          </a>
+          <br />
+          <span>
+            Multiple Modules | <b>Not available until</b> May 10 at 12:00am |<br /> <b>Due</b> May
+            27 at 11:59pm | 100 pts
+          </span>
+        </li>
+      </ul>
     </div>
   );
 }
