3a647b6bb096600fb4de5880fd7ade8049daa6fd
Ritish Reddy <80112277+ritish1082@users.noreply.github.com>
2024-10-15 18:10:15 -0400

    Revert "Merge A3 into main branch"

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,25 @@
 import React from "react";
-import Kanbas from "./Kanbas";
-import Labs from "./Labs";
-import {HashRouter, Route, Routes, Navigate } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
-
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <HashRouter>
-      <div>
-        <Routes>
-          <Route path="/" element={<Navigate to="/Labs" />} />
-          <Route path="/Labs/*" element={<Labs />} />
-          <Route path="/Kanbas/*" element={<Kanbas />} />
-        </Routes>
-      </div>
-    </HashRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
