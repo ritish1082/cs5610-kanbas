1dd48bd0a0bbd9f1a505fa2c4222679ec855c90c
ritish <sritish.10@gmail.com>
2024-10-16 01:18:31 -0400

    fix: Assignment contol button

--- src/Kanbas/Courses/Home/Status.tsx
+++ src/Kanbas/Courses/Home/Status.tsx
@@ -6,7 +6,7 @@ import { FaHome, FaBullhorn, FaChartLine, FaBell } from "react-icons/fa"; // Imp
 
 export default function CourseStatus() {
   return (
-    <div id="wd-course-status" style={{ width: "300px" }}>
+    <div id="wd-course-status" style={{ width: "300px" }} className="d-none d-xl-block" >
       <h2>Course Status</h2>
       <div className="d-flex">
         <div className="w-50 pe-1">
