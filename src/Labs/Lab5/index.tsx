eb9eb4a8946ab12d9cc90b6db5ffb24dd5e4fe23
ritish <sritish.10@gmail.com>
2024-11-23 01:16:28 -0500

    fix: deploy issues in render

--- src/Labs/Lab5/index.tsx
+++ src/Labs/Lab5/index.tsx
@@ -1,8 +1,11 @@
 import EnvironmentVariables from "./EnvironmentVariables";
+import HttpClient from "./HttpClient";
 import PathParameters from "./PathParameters";
 import QueryParameters from "./QueryParameters";
 import WorkingWithArrays from "./WorkingWithArrays";
+import WorkingWithArraysAsynchronously from "./WorkingWithArraysAsynchronously";
 import WorkingWithObjects from "./WorkingWithObjects";
+import WorkingWithObjectsAsynchronously from "./WorkingWithObjectsAsynchronously";
 const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 
 export default function Lab5() {
@@ -18,8 +21,11 @@ export default function Lab5() {
       <EnvironmentVariables />
       <PathParameters />
       <QueryParameters />
-      <WorkingWithObjects />
+      <HttpClient />
       <WorkingWithArrays />
+      <WorkingWithArraysAsynchronously />
+      <WorkingWithObjects />
+      <WorkingWithObjectsAsynchronously />
     </div>
   );
 }
